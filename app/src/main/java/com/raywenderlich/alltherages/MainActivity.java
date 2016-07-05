a583ecbcf0e90d408e900c07631222302e3ca2d8
Franco <frfranco@uci.edu>
2016-07-05 11:44:44 -0700

    communitcatin with the activity

--- app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
+++ app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
@@ -24,18 +24,26 @@ package com.raywenderlich.alltherages;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity
+        implements RageComicListFragment.OnRageComicSelected {
 
-    if (savedInstanceState == null) {
-      getSupportFragmentManager()
-              .beginTransaction()
-              .add(R.id.root_layout, RageComicListFragment.newInstance(), "rageComicList")
-              .commit();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.root_layout, RageComicListFragment.newInstance(), "rageComicList")
+                    .commit();
+        }
     }
-  }
+
+    @Override
+    public void onRageComicSelected(int imageResId, String name, String description, String url) {
+        Toast.makeText(this, "Hey, you selected " + name + "!", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
+++ app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
@@ -45,6 +45,8 @@ public class RageComicListFragment extends Fragment {
     private String[] mDescriptions;
     private String[] mUrls;
 
+    private OnRageComicSelected mListener;
+
     public static RageComicListFragment newInstance() {
         return new RageComicListFragment();
     }
@@ -56,6 +58,12 @@ public class RageComicListFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
+        if (context instanceof OnRageComicSelected) {
+            mListener = (OnRageComicSelected) context;
+        } else {
+            throw new ClassCastException(context.toString() + " must implement OnRageComicSelected.");
+        }
 
         // Get rage face names and descriptions.
         final Resources resources = context.getResources();
@@ -106,6 +114,13 @@ public class RageComicListFragment extends Fragment {
             final String description = mDescriptions[position];
             final String url = mUrls[position];
             viewHolder.setData(imageResId, name);
+
+            viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mListener.onRageComicSelected(imageResId, name, description, url);
+                }
+            });
         }
 
         @Override
@@ -132,4 +147,10 @@ public class RageComicListFragment extends Fragment {
             mNameTextView.setText(name);
         }
     }
+
+    public interface OnRageComicSelected {
+        void onRageComicSelected(int imageResId, String name,
+                                 String description, String url);
+    }
+
 }
