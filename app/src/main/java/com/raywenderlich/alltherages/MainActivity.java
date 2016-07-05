8088ccfde1082e9f8a5941780b5f25520d59f0fa
Franco <frfranco@uci.edu>
2016-07-05 11:38:33 -0700

    adding fragments dynamically

--- app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
+++ app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
@@ -30,5 +30,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+              .beginTransaction()
+              .add(R.id.root_layout, RageComicListFragment.newInstance(), "rageComicList")
+              .commit();
+    }
   }
 }
--- app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
+++ app/src/main/java/com/raywenderlich/alltherages/RageComicListFragment.java
@@ -22,8 +22,14 @@
 
 package com.raywenderlich.alltherages;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,66 +37,99 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 public class RageComicListFragment extends Fragment {
 
-  private int[] mImageResIds;
-  private String[] mNames;
-  private String[] mDescriptions;
-  private String[] mUrls;
+    private int[] mImageResIds;
+    private String[] mNames;
+    private String[] mDescriptions;
+    private String[] mUrls;
 
-  public static RageComicListFragment newInstance() {
-    return new RageComicListFragment();
-  }
+    public static RageComicListFragment newInstance() {
+        return new RageComicListFragment();
+    }
 
-  public RageComicListFragment() {
-    // Required empty public constructor
-  }
-
-  class RageComicAdapter extends RecyclerView.Adapter<ViewHolder> {
-
-    private LayoutInflater mLayoutInflater;
-
-    public RageComicAdapter(Context context) {
-      mLayoutInflater = LayoutInflater.from(context);
+    public RageComicListFragment() {
+        // Required empty public constructor
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-      return new ViewHolder(mLayoutInflater
-          .inflate(R.layout.recycler_item_rage_comic, viewGroup, false));
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // Get rage face names and descriptions.
+        final Resources resources = context.getResources();
+        mNames = resources.getStringArray(R.array.names);
+        mDescriptions = resources.getStringArray(R.array.descriptions);
+        mUrls = resources.getStringArray(R.array.urls);
+
+        // Get rage face images.
+        final TypedArray typedArray = resources.obtainTypedArray(R.array.images);
+        final int imageCount = mNames.length;
+        mImageResIds = new int[imageCount];
+        for (int i = 0; i < imageCount; i++) {
+            mImageResIds[i] = typedArray.getResourceId(i, 0);
+        }
+        typedArray.recycle();
     }
 
+    @Nullable
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-      final int imageResId = mImageResIds[position];
-      final String name = mNames[position];
-      final String description = mDescriptions[position];
-      final String url = mUrls[position];
-      viewHolder.setData(imageResId, name);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_rage_comic_list, container, false);
+
+        final Activity activity = getActivity();
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(activity, 2));
+        recyclerView.setAdapter(new RageComicAdapter(activity));
+        return view;
     }
 
-    @Override
-    public int getItemCount() {
-      return mNames.length;
+    class RageComicAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        private LayoutInflater mLayoutInflater;
+
+        public RageComicAdapter(Context context) {
+            mLayoutInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            return new ViewHolder(mLayoutInflater
+                    .inflate(R.layout.recycler_item_rage_comic, viewGroup, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+            final int imageResId = mImageResIds[position];
+            final String name = mNames[position];
+            final String description = mDescriptions[position];
+            final String url = mUrls[position];
+            viewHolder.setData(imageResId, name);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mNames.length;
+        }
     }
-  }
 
-  class ViewHolder extends RecyclerView.ViewHolder {
-    // Views
-    private ImageView mImageView;
-    private TextView mNameTextView;
+    class ViewHolder extends RecyclerView.ViewHolder {
+        // Views
+        private ImageView mImageView;
+        private TextView mNameTextView;
 
-    private ViewHolder(View itemView) {
-      super(itemView);
+        private ViewHolder(View itemView) {
+            super(itemView);
 
-      // Get references to image and name.
-      mImageView = (ImageView) itemView.findViewById(R.id.comic_image);
-      mNameTextView = (TextView) itemView.findViewById(R.id.name);
+            // Get references to image and name.
+            mImageView = (ImageView) itemView.findViewById(R.id.comic_image);
+            mNameTextView = (TextView) itemView.findViewById(R.id.name);
+        }
+
+        private void setData(int imageResId, String name) {
+            mImageView.setImageResource(imageResId);
+            mNameTextView.setText(name);
+        }
     }
-
-    private void setData(int imageResId, String name) {
-      mImageView.setImageResource(imageResId);
-      mNameTextView.setText(name);
-    }
-  }
 }
